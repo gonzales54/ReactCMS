7471ff6864a2bc387da3ec9aad8445b3c8160c8a
gonzales <gonzalesitokawa@gmail.com>
2022-11-26 12:18:01 +0900

    add: edit, newpost tsx

--- src/src/components/views/EditPost.tsx
+++ src/src/components/views/EditPost.tsx
@@ -5,7 +5,12 @@ export const EditPost = () => {
   const path: string = useSelector<RootState, string>(state => state.getPath.pathname)
   return (
     <main className='main'>
-      <h1>EditPost</h1>
+      <form className='form'>
+        <h2 className='sectionTitle'>Edit Post</h2>
+        <input type="text" name="" id="" defaultValue="Link"/>
+        <textarea name="teextarea" id="" cols={30} rows={10} defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."></textarea>
+        <button type="submit" className='submitBtn'>Submit</button>
+      </form>
     </main>
   ) 
 }
